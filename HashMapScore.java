dc682adc702f7807e97ae8aa6d5577cb68a61f90
iwamegu1208 <iwamegu1208@yahoo.co.jp>
2019-10-07 13:49:59 +0900

    test1를 치웠습니다.

---
+++ HashMapScore.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+/**
+ * Write a description of class HashMapScore here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class HashMapScore
+{
+    public static void main(String[] args){
+        Hash<String, Integer> scoreMap = new Hash<String, Integer>();
+        
+        
+}
